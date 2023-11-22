7d118d13e1092fe79a300b50162bd4c01b904496
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-11-22 12:03:27 +0100

    Config initiation

---
+++ src/arduino/src/config.h
@@ -0,0 +1,25 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define LED_1 2
+#define LED_2 3
+#define LED_3 4
+#define BUT_1 5
+#define TEMP_PIN 7
+#define TRIG_PIN 8
+#define ECHO_PIN 9
+#define GATE_PIN 10
+#define PIR_PIN 11
+#define SDA_PIN A0
+#define SCL_PIN A1
+
+#define N1 3
+#define N2 4
+#define N3 10
+#define N4 2
+
+#define MINDIST
+#define MAXDIST
+#define MAXTEMP
+
+#endif
