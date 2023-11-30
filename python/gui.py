bf40c3bdd9a6c5ea56aee28183009fbadc092a90
Saint138 <93931915+Saint138@users.noreply.github.com>
2023-11-30 23:58:05 +0100

    modifiche

--- python/gui.py
+++ python/gui.py
@@ -7,80 +7,75 @@ state_strings = ['SLEEPING', 'CHECK_IN', 'ENTERING', 'READY', 'WASHING', 'ERROR'
 
 class ArduinoCommunication:
     def __init__(self, port, baud_rate=9600):
-        self.serial_port = serial.Serial(port, baud_rate)
-        self.data = {'state': '', 'temperature': '', 'distance': ''}
-        self.is_reading = False
-        self.read_thread = threading.Thread(target=self.read_data_thread)
-        self.read_thread.start()
-        self.wash_count = 0
+        self.serial_port = serial.Serial(port, baud_rate) # apre la porta seriale
+        self.data = {'state': '', 'temperature': '', 'distance': ''} # inizializza il dizionario
+        self.is_reading = False # variabile per il thread
+        self.read_thread = threading.Thread(target=self.read_data_thread) # crea il thread
+        self.read_thread.start() # avvia il thread
+        self.wash_count = 0 # variabile per il conteggio dei lavaggi
         self.wash_in_progress = False  # Variable to track if a wash is in progress
 
         # Create a label for maintenance message
-        self.maintenance_label = tk.Label(root, text="Maintenance required", fg="red", font=("Helvetica", 14))
-        self.maintenance_label.pack(padx=10, pady=5, anchor='w')
-        self.update_maintenance_label()
+        self.maintenance_label = tk.Label(root, text="Maintenance required", fg="red", font=("Helvetica", 14)) # crea il label, ma non lo mostra
+        self.maintenance_label.pack(padx=10, pady=5, anchor='w') #aggiustamenti grafici
+        self.update_maintenance_label() # aggiorna il label
 
-    def read_data_thread(self):
-        self.is_reading = True
-        while self.is_reading:
-            if self.serial_port.in_waiting > 0:
-                data = self.serial_port.readline().decode().strip().split(':')
-                if len(data) == 3:
-                    self.data['state'], self.data['temperature'], self.data['distance'] = data
-                    self.update_gui()
+    def read_data_thread(self): # funzione per leggere i dati dal thread
+        self.is_reading = True # variabile per il thread
+        while self.is_reading: # ciclo while per leggere i dati
+            if self.serial_port.in_waiting > 0: 
+                data = self.serial_port.readline().decode().strip().split(':') # legge i dati dalla seriale, li decodifica e li divide in una lista
+                if len(data) == 3: 
+                    self.data['state'], self.data['temperature'], self.data['distance'] = data # se i dati sono 3 li assegna al dizionario
+                    self.update_gui() # aggiorna la gui
 
     def update_gui(self):
-        state_label.config(text=f"State: {self.data['state']}")
-        temp_label.config(text=f"Temperature: {self.data['temperature']} °C")
-        distance_label.config(text=f"Distance: {self.data['distance']} units")
-        wash_count_label.config(text=f"Wash Count: {self.wash_count}")
+        state_label.config(text=f"State: {self.data['state']}") # aggiorna i label, inserendo i dati
+        temp_label.config(text=f"Temperature: {self.data['temperature']} °C") # aggiorna i label, inserendo i dati
+        distance_label.config(text=f"Distance: {self.data['distance']} metri") # aggiorna i label, inserendo i dati
+        wash_count_label.config(text=f"Wash Count: {self.wash_count}") # aggiorna i label, inserendo i dati
 
         # Check if a wash is in progress and has just finished
         if self.wash_in_progress and self.data['state'] == 'FINISHED':
-            self.increase_wash_count()
-            self.wash_in_progress = False
+            self.increase_wash_count() #incrementa il conteggio dei lavaggi
+            self.wash_in_progress = False # resetta la variabile
         elif self.data['state'] == 'WASHING':
             self.wash_in_progress = True
 
-        self.update_maintenance_label()
+        self.update_maintenance_label() 
 
-    def stop_reading(self):
+    def stop_reading(self): # funzione per fermare il thread
         self.is_reading = False
         self.read_thread.join()
         self.serial_port.close()
 
-    def increase_wash_count(self):
+    def increase_wash_count(self): # funzione per incrementare il conteggio dei lavaggi
         # Increment the wash count
         self.wash_count += 1
 
-    def send_maintenance_done(self):
-        # Send "Maintenance done" message to Arduino
+    def send_maintenance_done(self): # funzione per inviare il messaggio di manutenzione
         self.serial_port.write("Maintenance done\n".encode())
 
-    def show_maintenance_label(self):
-        # Show the maintenance label when maintenance is required
+    def show_maintenance_label(self): #funzionw per mostrare il label di manutenzione
         self.maintenance_label.pack()
 
-    def hide_maintenance_label(self):
-        # Hide the maintenance label when maintenance is not required
+    def hide_maintenance_label(self): # funzione per nascondere il label di manutenzione
         self.maintenance_label.pack_forget()
 
-    def update_maintenance_label(self):
-        # Update the maintenance label based on the current state
+    def update_maintenance_label(self): # funzione per aggiornare il label di manutenzione
         if self.data['state'] == 'ERROR':
             self.show_maintenance_label()
         else:
             self.hide_maintenance_label()
 
-# Function to be called when the button is clicked
-def on_button_click():
+def on_button_click(): # funzione per il click del bottone
     if arduino.data['state'] == 'ERROR':
         arduino.send_maintenance_done()
         arduino.hide_maintenance_label()
 
-# Create the tkinter master object
+#crea la finestra
 root = tk.Tk()
-root.title('Smart Bridge')
+root.title('SmartCarWash')
 
 # Create labels for displaying data
 state_label = tk.Label(root, text="State: ", anchor='w', font=("Helvetica", 14))
